d48070bdfde748414b6c5f2db818f7d083a608d5
jarvis <jarvis@jarviss-mbp>
2018-07-31 08:58:55 +0300

    initial commit

---
+++ src/teamproject/TeamProject.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamproject;
+
+/**
+ *
+ * @author jarvis
+ */
+public class TeamProject {
+    
+    public void func1(){
+        
+        int x = func2();
+        
+        int y = func3();
+        
+    }
+    
+    public int func2(){
+        
+        return 2;
+    }
+    
+    public int func3(){
+        
+        return 3;
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
